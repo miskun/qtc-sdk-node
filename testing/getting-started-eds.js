c432bfb389484901173a9121d60027c0e94818fc
miskun <miska.kaipiainen@gmail.com>
2014-03-22 14:10:35 +0200

    preparing to add MWS support

---
+++ testing/getting-started-eds.js
@@ -0,0 +1,31 @@
+var Qtc = require("../lib/qtc.js");
+
+// NOTE! Create settings.js file to use this example. You can use settings-default.js as a template
+var ENV = require("./settings.js");
+
+var eds = new Qtc.Eds({
+    address: ENV.eds.address,
+    backendId: ENV.eds.backendId,
+    secret: ENV.eds.secret
+});
+
+// Define collection used in this example
+var contacts = eds.collection("contacts");
+contacts.debug = true;
+
+// Insert object to EDS
+contacts.insert({
+    name: "John",
+    age: 31,
+    likes: ["pizza", "coke"],
+    address: {
+        city: "Springfield",
+        country: "USA"
+    }
+}, function(e, result){
+    if(!e){
+        console.log(result);
+    } else {
+        console.log("Ooops! Something went wrong!", e);
+    }
+});
---
+++ testing/settings-default.js
@@ -0,0 +1,14 @@
+// Add your info below and rename to settings.js
+
+module.exports = {
+    eds: {
+        address: "YOUR_EDS_INSTANCE_ADDRESS",           // copy/paste your eds address here
+        backendId: "YOUR_EDS_INSTANCE_BACKEND_ID",      // copy/paste your eds backend id here
+        secret: "YOUR_EDS_INSTANCE_SECURITY_TOKEN"      // copy/paste your eds security token here (or "")
+    },
+    mws: {
+        address: "YOUR_MWS_INSTANCE_ADDRESS",           // copy/paste your mws address here
+        socketId: "YOUR_MWS_INSTANCE_SOCKET_ID",        // copy/paste your mws socket id here
+        secret: "YOUR_MWS_INSTANCE_SECURITY_TOKEN"      // copy/paste your mws security token here (or "")
+    }
+};
